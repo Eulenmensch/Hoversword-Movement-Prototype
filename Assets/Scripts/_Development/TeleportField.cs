fbc0f38671651e93b6a0a5544a2ee57a6398e683
mimro <m_mros@gmx.net>
2020-05-19 17:36:26 +0200

    Started to set up a Level Design Prototyping scene

---
+++ Assets/Scripts/_Development/TeleportField.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportField : MonoBehaviour, ICollidable
+{
+    public Transform target;
+
+    public CollisionInteraction Collide()
+    {
+        CollisionInteraction collisionInteraction = new CollisionInteraction(false);
+        collisionInteraction.SetTeleport(true, target.position);
+        return collisionInteraction;
+    }
+}
