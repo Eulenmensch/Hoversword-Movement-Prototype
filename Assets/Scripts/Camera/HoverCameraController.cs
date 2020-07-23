f90e1c7154588b687935fbc47d04eac66bd46cc8
Yoshua Woo <yoshua.woo@gmail.com>
2020-07-23 12:39:47 +0200

    Camera now moves slightly into the direction the player is steering in; adjusted prefabs for the change;

--- Assets/Scripts/Camera/HoverCameraController.cs
+++ Assets/Scripts/Camera/HoverCameraController.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
+using UnityEditor;
 using Cinemachine;
-using DG.Tweening;
+using UnityEngine.InputSystem;
 
 public class HoverCameraController : MonoBehaviour
 {
@@ -8,27 +9,55 @@ public class HoverCameraController : MonoBehaviour
     [SerializeField] private PlayerHandling Handling;
     [SerializeField] private Rigidbody RB;
     [SerializeField] float MaxZOffset;
+    [SerializeField] float MaxXOffset;
+    // [SerializeField] float ZLerpTime;
+    [SerializeField] float XLerpTime;
+    [SerializeField] AnimationCurve FOVCurve;
 
     private float ZOffset;
+    private float XOffset;
     private Vector3 TrackedObjectOffset;
 
     private void FixedUpdate()
     {
         ScaleZOffsetWithSpeed();
+        SetOffset(XOffset, ZOffset);
     }
 
     private void ScaleZOffsetWithSpeed()
     {
-        PhysicsUtilities.ScaleForceWithSpeed( ref ZOffset, 0, MaxZOffset, RB, Handling.MaxSpeed );
-        TrackedObjectOffset = new Vector3( 0, 1, ZOffset );
+        PhysicsUtilities.ScaleForceWithSpeed(ref ZOffset, 0, MaxZOffset, RB, Handling.MaxSpeed);
+    }
 
-        for ( int i = 0; i < 3; i++ )
+    public void ScaleXOffsetWithTurnInput(InputAction.CallbackContext context)
+    {
+        Vector2 inputVector = context.ReadValue<Vector2>();
+        XOffset = inputVector.x * MaxXOffset;
+    }
+
+    private void SetOffset(float _xOffset, float _zOffset)
+    {
+        TrackedObjectOffset.x = Mathf.Lerp(TrackedObjectOffset.x, XOffset, XLerpTime);
+        TrackedObjectOffset.y = 1;
+        TrackedObjectOffset.z = ZOffset;
+
+        for (int i = 0; i < 3; i++)
         {
-            var rig = FreeLook.GetRig( i );
+            var rig = FreeLook.GetRig(i);
             var composer = rig.GetCinemachineComponent<CinemachineComposer>();
             composer.m_TrackedObjectOffset = TrackedObjectOffset;
         }
     }
 
+    private void PulseFOV()
+    {
 
+    }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(RB.transform.TransformPoint(TrackedObjectOffset), 0.2f);
+    }
+#endif
 }
