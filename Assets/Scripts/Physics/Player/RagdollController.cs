1761f7da012261ea45f49be41c7ec3283464440d
mimro <m_mros@gmx.net>
2020-08-19 13:59:08 +0200

    Collision crashing first implementation

--- Assets/Scripts/Physics/Player/RagdollController.cs
+++ Assets/Scripts/Physics/Player/RagdollController.cs
@@ -9,6 +9,19 @@ public class RagdollController : MonoBehaviour
     private Rigidbody[] Rigidbodies;
     private Rigidbody RB;
 
+    [SerializeField] private bool _crashOnCollision;
+    [SerializeField] private float _forceMin = 20f;
+    [SerializeField] private float _forceMax = 40f;
+    [SerializeField] private float _upMin = 0.65f;
+    [SerializeField] private float _upMax = 1f;
+    [SerializeField] private float _frontMin = 0.4f;
+    [SerializeField] private float _frontMax = 0.8f;
+    [SerializeField] private float _sumThresold = 1.6f;
+
+    //[SerializeField] private float _frontalThreshold;
+    //[SerializeField] private float _upThreshold;
+    //[SerializeField] private float _forceThreshold;
+
     private void Start()
     {
         Rigidbodies = Character.GetComponentsInChildren<Rigidbody>();
@@ -34,11 +47,10 @@ public class RagdollController : MonoBehaviour
         Character.GetComponent<Animator>().enabled = false;
         foreach (var body in Rigidbodies)
         {
-            var forceMultiplier = Random.Range(-2f, 5f);
-            body.AddForce(_crashForce * forceMultiplier, ForceMode.VelocityChange);
+            //var forceMultiplier = Random.Range(0.8f, 1.2f);
+            body.AddForce(_crashForce/* * forceMultiplier*/, ForceMode.VelocityChange);
         }
     }
-
 
     private void SetRigidbodiesKinematic(bool _isKinematic)
     {
@@ -53,6 +65,43 @@ public class RagdollController : MonoBehaviour
         foreach (var collider in Colliders)
         {
             collider.enabled = _isEnabled;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!_crashOnCollision)
+            return;
+
+        ContactPoint[] contactPoints = new ContactPoint[collision.contactCount];
+        collision.GetContacts(contactPoints);
+
+        foreach (var contactPoint in contactPoints)
+        {
+            DebugExtension.DebugPoint(contactPoint.point, Color.black, duration: 10f);
+            Debug.DrawLine(contactPoint.point, contactPoint.point + contactPoint.normal, Color.grey, duration: 10f);
+            //Debug.DrawLine(contactPoints[0].point, contactPoints[0].point + relativeVelocity, Color.red, duration: 10f);
+        }
+
+        var relativeVelocity = collision.relativeVelocity;
+        var force = relativeVelocity.magnitude;
+        var normal = contactPoints[0].normal;
+        //var impulse = collision.impulse.magnitude;
+
+        var dotFront = Vector3.Dot(normal, -transform.forward);
+        var dotUp = Vector3.Dot(normal, transform.up);
+        //print($"up: {dotUp}; front: {dotFront}; force: {force}; impulse: {impulse}");
+
+        float forceWeight = Utility.RemapNumber(force, _forceMin, _forceMax, 0f, 1f);
+        float upWeight = Utility.RemapNumberClamped(dotUp, _upMin, _upMax, 0, -1f);
+        float frontWeight = Utility.RemapNumber(dotFront, _frontMin, _frontMax, 0f, 1f);
+        float sum = forceWeight + upWeight + frontWeight;
+        //print($"upW: {upWeight}; frontW: {frontWeight}; forceW: {forceWeight}; sum: {sum}");
+
+        if (sum > _sumThresold)
+        {
+            print("CRASH");
+            RagdollCrash(-relativeVelocity * 1.5f);
         }
     }
 }
